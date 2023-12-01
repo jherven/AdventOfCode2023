c82c9d443936068e9b9cb631eb8d9673792b64e4
Joel Hervén <joel@herven.ninja>
2023-12-01 17:33:30 +0100

    Day 1 part 1

---
+++ Day01/Program.cs
@@ -0,0 +1,7 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var result = lines.Sum(line => {
+	var digits = line.Where(c => Char.IsDigit(c)).ToList();
+	return Convert.ToInt32(digits.First() + "" + digits.Last());
+});
+Console.WriteLine(result);
+
