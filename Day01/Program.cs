abbec4537989e5b2a6516e558a82bf9b0433d546
Joel Hervén <joel@herven.ninja>
2023-12-01 21:15:23 +0100

    1b

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,7 +1,42 @@
-﻿var lines = File.ReadAllLines("input.txt");
+﻿string[] letterNumbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+var lines = File.ReadAllLines("input.txt");
 var result = lines.Sum(line => {
-	var digits = line.Where(c => Char.IsDigit(c)).ToList();
-	return Convert.ToInt32(digits.First() + "" + digits.Last());
+	var digits1 = ReplaceFirstLetterOccurence(line).Where(c => Char.IsDigit(c)).ToList();
+	var digits2 = ReplaceLastLetterOccurence(line).Where(c => Char.IsDigit(c)).ToList();
+	return Convert.ToInt32(digits1.First() + "" + digits2.Last());
 });
 Console.WriteLine(result);
+
+string ReplaceFirstLetterOccurence(string row) {
+	var part = "";
+	foreach (var letter in row)
+	{
+		if (Char.IsDigit(letter))
+			break;
+		part += letter;
+		for (var i = 0; i < letterNumbers.Length; i++)
+		{
+			var ln = letterNumbers[i];
+			if (part.EndsWith(ln))
+				return row.Replace(ln, (i + 1).ToString());
+		}
+	}
+	return row;
+}
+
+string ReplaceLastLetterOccurence(string row) {
+	var part = "";
+	foreach (var letter in row.Reverse()) {
+		if (Char.IsDigit(letter))
+			break;
+		part = letter + part;
+		for (var i = 0; i < letterNumbers.Length; i++)
+		{
+			var ln = letterNumbers[i];
+			if (part.StartsWith(ln))
+				return row.Replace(ln, (i + 1).ToString());
+		}
+	}
+	return row;
+}
 
