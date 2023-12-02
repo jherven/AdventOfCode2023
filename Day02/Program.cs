1cdb2b2a2c4c34b667ef09425775195ac1102ca5
Joel Hervén <joel@herven.ninja>
2023-12-02 23:49:56 +0100

    Day2 part 1

---
+++ Day02/Program.cs
@@ -0,0 +1,38 @@
+﻿var inventory = new Dictionary<CubeType, int>()
+{
+    { CubeType.Red, 12 },
+    { CubeType.Green, 13 },
+    { CubeType.Blue, 14 },
+};
+
+var lines = File.ReadAllLines("input.txt");
+int valid = 0;
+int i = 1;
+foreach (var line in lines)
+{
+    var parsedLine = ParseLine(line);
+    if (parsedLine.red <= inventory[CubeType.Red] && parsedLine.green <= inventory[CubeType.Green] &&
+        parsedLine.blue <= inventory[CubeType.Blue])
+        valid+= i;
+    i++;
+}
+
+Console.WriteLine(valid);
+
+(int red, int green, int blue) ParseLine(string line)
+{
+    line = line.Split(':')[1];
+    var lineParts = line.Split(';').SelectMany(l => l.Trim().Split(',').Select(t => t.Trim())).ToList();
+    return (
+        lineParts.Where(c => c.EndsWith(CubeType.Red.ToString().ToLower())).Select(s => Convert.ToInt32(s.Split(' ')[0])).DefaultIfEmpty(0).Max(),
+        lineParts.Where(c => c.EndsWith(CubeType.Green.ToString().ToLower())).Select(s => Convert.ToInt32(s.Split(' ')[0])).DefaultIfEmpty(0).Max(),
+        lineParts.Where(c => c.EndsWith(CubeType.Blue.ToString().ToLower())).Select(s => Convert.ToInt32(s.Split(' ')[0])).DefaultIfEmpty(0).Max()
+    );
+}
+
+enum CubeType
+{
+    Red,
+    Green,
+    Blue,
+}
